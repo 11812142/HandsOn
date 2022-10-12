6619e3811156c118879da65f89466752aea1c711
Pratibha Sheoran <pratibhasheoran2000@gmail.com>
2022-10-12 23:57:27 +0530

    new

---
+++ ServiceLifeTime/ServiceLifeTime/Models/OperationServices.cs
@@ -0,0 +1,30 @@
+﻿namespace ServiceLifeTime.Models;
+using System.Security.Cryptography.X509Certificates;
+
+    public class OperationServices : ISingletonService, ITransientService, IScopedService
+    {
+        Guid id;
+        int no;
+        public OperationServices()
+        {
+
+            id = Guid.NewGuid();
+            Random r = new Random();
+            no = r.Next(1200);
+
+
+        }
+
+        public Guid GetOperationID()
+        {
+
+            return id;
+        }
+        public int GenerateRandomNumber()
+        {
+            return no;
+        }
+
+
+    }
+
